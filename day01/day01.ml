f1e898dc7388f8758e5c11a47341184e1707cbba
Walid Menou <walid.menou@outlook.com>
2025-12-20 22:04:43 -0500

    day 1

--- day01/day01.ml
+++ day01/day01.ml
@@ -1,33 +1,40 @@
-open Aoc2025;;
-open Parser;;
+open Aoc2025
+open Parser
 
 let rotation =
   alt (char 'L') (char 'R')
-  |*> fun d -> number
-  |*> fun n -> maybe (char '\n')
-  |*> fun _ -> of_value (if d = 'R' then n else -n)
+  |*> fun d ->
+  number |*> fun n -> maybe (char '\n') |*> fun _ -> of_value (if d = 'R' then n else -n)
 ;;
 
-let zeros =
+let part1 =
+  let rec helper pos count =
+  alt
+    (rotation
+      |*> fun n ->
+          let pos = (pos + n) mod 100 in
+          if pos = 0 then helper pos (count + 1) else helper pos count)
+    (of_value count)
+  in
+  helper 50 0
+
+let part2 =
   let rec helper pos count =
     alt
-    (rotation
-    |*> fun n ->
-        let new_pos = pos + n in
-        let count = count + Int.abs((new_pos / 100) - (pos / 100)) in
-        helper new_pos count)
-    (of_value count)
+      (rotation
+      |*> fun n ->
+          let pos = pos + n in
+          let count = count + (Int.abs(pos) / 100) + Bool.to_int(pos <= 0 && pos - n <> 0) in
+          let pos = (pos mod 100 + 100) mod 100 in
+          helper pos count)
+      (of_value count)
   in
   helper 50 0
 ;;
 
+let input = In_channel.stdin |> In_channel.input_all |> stol
 
 let _ =
-  In_channel.stdin
-  |> In_channel.input_all
-  |> stol
-  |> zeros
-  |> Option.get
-  |> fst
-  |> Int.to_string
-  |> print_endline
+  input |> part1 |> Option.get |> fst |> Int.to_string |> print_endline;
+  input |> part2 |> Option.get |> fst |> Int.to_string |> print_endline
+;;
